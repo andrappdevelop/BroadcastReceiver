c192df7b62dfc516be0d47510ebdbcc162f566d4
IvanUsatov <andrappdevelop@gmail.com>
2023-01-31 18:48:35 +0700

    Add progressBar imitated download process

--- app/src/main/java/com/example/broadcastreceiver/MainActivity.kt
+++ app/src/main/java/com/example/broadcastreceiver/MainActivity.kt
@@ -1,19 +1,32 @@
 package com.example.broadcastreceiver
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ProgressBar
 
 class MainActivity : AppCompatActivity() {
 
-    private val receiver = MyReceiver()
+    private lateinit var progressBar: ProgressBar
+
+    private val receiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            if (intent.action == "loaded") {
+                val percent = intent.getIntExtra("percent", 0)
+                progressBar.progress = percent
+            }
+        }
+    }
     private var countOfClicks = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        progressBar = findViewById(R.id.progressBar)
         findViewById<Button>(R.id.button).setOnClickListener {
             Intent(MyReceiver.ACTION_CLICKED).apply {
                 putExtra(MyReceiver.EXTRA_COUNT, countOfClicks++)
@@ -21,11 +34,12 @@ class MainActivity : AppCompatActivity() {
             }
         }
         val intentFilter = IntentFilter().apply {
-            addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
-            addAction(Intent.ACTION_POWER_CONNECTED)
-            addAction(MyReceiver.ACTION_CLICKED)
+            addAction("loaded")
         }
         registerReceiver(receiver, intentFilter)
+        Intent(this, MyService::class.java).apply {
+            startService(this)
+        }
     }
 
     override fun onDestroy() {
--- app/src/main/java/com/example/broadcastreceiver/MyReceiver.kt
+++ app/src/main/java/com/example/broadcastreceiver/MyReceiver.kt
@@ -8,6 +8,14 @@ import android.widget.Toast
 class MyReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         when (intent?.action) {
+            "loaded" -> {
+                val percent = intent.getIntExtra("percent", 0)
+                Toast.makeText(
+                    context,
+                    "Loaded $percent",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
             ACTION_CLICKED -> {
                 val countOfClicks = intent.getIntExtra(EXTRA_COUNT, 0)
                 Toast.makeText(
